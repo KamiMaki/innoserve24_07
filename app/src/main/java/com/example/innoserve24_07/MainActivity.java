01bea640b15921c19146e7343054c51d19e205ba
Dorggie <52898963+Dorggie@users.noreply.github.com>
2019-07-17 09:35:32 +0800

    add TV

--- app/src/main/java/com/example/innoserve24_07/MainActivity.java
+++ app/src/main/java/com/example/innoserve24_07/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
