159fb186299db724fa2ca72c15d149e6e6ad386a
jasminecpc <52899000+jasminecpc@users.noreply.github.com>
2019-07-17 09:53:01 +0800

    add main button

--- app/src/main/java/com/example/innoserve24_07/MainActivity.java
+++ app/src/main/java/com/example/innoserve24_07/MainActivity.java
@@ -7,19 +7,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-
+    Button bt1;
+    Button bt2;
+    Button bt3;
+    Button bt4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,webview_bot.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
