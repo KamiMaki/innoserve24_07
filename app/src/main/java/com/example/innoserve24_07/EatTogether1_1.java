3dbbbf643cea419d084d2458475eb8a8d32db523
jasminecpc <52899000+jasminecpc@users.noreply.github.com>
2019-07-22 12:05:07 +0800

    close

--- app/src/main/java/com/example/innoserve24_07/EatTogether1_1.java
+++ app/src/main/java/com/example/innoserve24_07/EatTogether1_1.java
@@ -53,7 +53,26 @@ public class EatTogether1_1 extends AppCompatActivity
     private Marker mPingChen9;
     private static final LatLng PingChen10 = new LatLng(24.951171, 121.206371 );
     private Marker mPingChen10;
-
+    private static final LatLng LungTan1 = new LatLng(24.845209, 121.153220 );
+    private Marker mLungTan1;
+    private static final LatLng LungTan2 = new LatLng(24.882359, 121.195308 );
+    private Marker mLungTan2;
+    private static final LatLng LungTan3 = new LatLng(24.881135, 121.212040 );
+    private Marker mLungTan3;
+    private static final LatLng LungTan4 = new LatLng(24.868270, 121.215471 );
+    private Marker mLungTan4;
+    private static final LatLng LungTan5 = new LatLng(24.864885, 121.217324 );
+    private Marker mLungTan5;
+    private static final LatLng LungTan6 = new LatLng(24.864500, 121.225853 );
+    private Marker mLungTan6;
+    private static final LatLng LungTan7 = new LatLng(24.875568, 121.228132 );
+    private Marker mLungTan7;
+    private static final LatLng LungTan8 = new LatLng(24.840931, 121.232381 );
+    private Marker mLungTan8;
+    private static final LatLng LungTan9 = new LatLng(24.842218, 121.241571 );
+    private Marker mLungTan9;
+    private static final LatLng LungTan10 = new LatLng(24.829095, 121.246823 );
+    private Marker mLungTan10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -144,11 +163,6 @@ public class EatTogether1_1 extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map=googleMap;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of abe6b9f... dot
         mPingChen1 = map.addMarker(new MarkerOptions()
                 .position(PingChen1));
         mPingChen2 = map.addMarker(new MarkerOptions()
@@ -180,6 +194,7 @@ public class EatTogether1_1 extends AppCompatActivity
         mPingChen8 = map.addMarker(new MarkerOptions().position(PingChen8));
         mPingChen9 = map.addMarker(new MarkerOptions().position(PingChen9));
         mPingChen10 = map.addMarker(new MarkerOptions().position(PingChen10));
+
         mLungTan1=map.addMarker((new MarkerOptions().position(LungTan1)));
         mLungTan2=map.addMarker((new MarkerOptions().position(LungTan2)));
         mLungTan3=map.addMarker((new MarkerOptions().position(LungTan3)));
@@ -190,11 +205,7 @@ public class EatTogether1_1 extends AppCompatActivity
         mLungTan8=map.addMarker((new MarkerOptions().position(LungTan8)));
         mLungTan9=map.addMarker((new MarkerOptions().position(LungTan9)));
         mLungTan10=map.addMarker((new MarkerOptions().position(LungTan10)));
-
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(PingChen1, 15));
->>>>>>> abe6b9fc9cf99ac0aa3c3ddb4dda82b74975d0ff
-=======
->>>>>>> parent of abe6b9f... dot
         map.setOnMarkerClickListener(this);
     }
 
@@ -240,9 +251,50 @@ public class EatTogether1_1 extends AppCompatActivity
         {
             myAlertDialog();
         }
+        if (marker.equals(mLungTan1))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan2))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan3))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan4))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan5))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan6))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan7))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan8))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan9))
+        {
+            myAlertDialog();
+        }
+        if (marker.equals(mLungTan10))
+        {
+            myAlertDialog();
+        }
         return false;
 
     }
+    //     點marker時跳出來的彈跳視窗
     private void myAlertDialog() {
         AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
         MyAlertDialog.setTitle("標題");
