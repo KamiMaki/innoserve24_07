822309175153e8c161d0987590cc814cf497f1d3
Dorggie <52898963+Dorggie@users.noreply.github.com>
2019-07-23 11:04:09 +0800

    gyug
    ygugi

---
+++ app/src/main/java/com/example/innoserve24_07/bg4_3question.java
@@ -0,0 +1,13 @@
+package com.example.innoserve24_07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bg4_3question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bg4_3question);
+    }
+}
