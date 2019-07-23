916e322a215fcbcb18e04314a425c7e958123c34
jasminecpc <52899000+jasminecpc@users.noreply.github.com>
2019-07-23 18:07:49 +0800

    flag

--- app/src/main/java/com/example/innoserve24_07/EatTogether1_1.java
+++ app/src/main/java/com/example/innoserve24_07/EatTogether1_1.java
@@ -76,7 +76,7 @@ public class EatTogether1_1 extends AppCompatActivity
     private Marker mLungTan9;
     private static final LatLng LungTan10 = new LatLng(24.829095, 121.246823 );
     private Marker mLungTan10;
-
+int flag=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,6 +95,8 @@ public class EatTogether1_1 extends AppCompatActivity
                 Intent intent = new Intent();
                 intent.setClass(EatTogether1_1.this,EatTogether1_1_add_activity.class);
                 startActivity(intent);
+                flag=1;
+
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -262,9 +264,7 @@ public class EatTogether1_1 extends AppCompatActivity
         mLungTan9=map.addMarker((new MarkerOptions().position(LungTan9)));
         mLungTan10=map.addMarker((new MarkerOptions().position(LungTan10)));
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(PingChen1, 15));
-
-
+       // map.moveCamera(CameraUpdateFactory.newLatLngZoom(PingChen1, 15));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(PingChen1, 13));
 
         map.setOnMarkerClickListener(this);
@@ -274,43 +274,73 @@ public class EatTogether1_1 extends AppCompatActivity
     public boolean onMarkerClick(Marker marker) {
         if (marker.equals(mPingChen1))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen2))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen3))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen4))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen5))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen6))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen7))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen8))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen9))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mPingChen10))
         {
-            myAlertDialog();
+            if(flag==0)
+                myAlertDialog();
+            else
+                mmyAlertDialog();
         }
         if (marker.equals(mLungTan1))
         {
@@ -358,8 +388,24 @@ public class EatTogether1_1 extends AppCompatActivity
     //     點marker時跳出來的彈跳視窗
     private void myAlertDialog() {
         AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
-        MyAlertDialog.setTitle("標題");
-        MyAlertDialog.setMessage("我是內容");
+        MyAlertDialog.setTitle("活動:");
+        MyAlertDialog.setMessage("目前無活動");
+        DialogInterface.OnClickListener OkClick = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // 如果不做任何事情 就會直接關閉 對話方塊
+            }
+        };
+        ;
+        MyAlertDialog.setNeutralButton("我要參加", OkClick);
+        MyAlertDialog.setPositiveButton("   關閉", OkClick);
+        MyAlertDialog.setNegativeButton("我感興趣    ", OkClick);
+        MyAlertDialog.show();
+
+    }
+    private void mmyAlertDialog() {
+        AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
+        MyAlertDialog.setTitle("活動:");
+        MyAlertDialog.setMessage("\n\n鄭奶奶揪吃飯\n\n活動地點:社區活動中心\n\n活動日期:108.07.28\n\n活動時間:11:30\n\n活動內容:大家每個人帶一道菜來，一人一道就有一桌菜了\n\n");
         DialogInterface.OnClickListener OkClick = new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 // 如果不做任何事情 就會直接關閉 對話方塊
