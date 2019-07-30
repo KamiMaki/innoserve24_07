f2b863aefd97ceb82501b852b04ef75fff4cd2b4
Dorggie <52898963+Dorggie@users.noreply.github.com>
2019-07-30 16:44:19 +0800

    aaa

--- app/src/main/java/com/example/innoserve24_07/bg4_3question.java
+++ app/src/main/java/com/example/innoserve24_07/bg4_3question.java
@@ -1,5 +1,7 @@
 package com.example.innoserve24_07;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,14 +17,17 @@ public class bg4_3question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bg4_3question);
         bt=(Button)findViewById(R.id.bt6);
-       // bt.setOnClickListener(listener);
+       bt.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               AlertDialog.Builder builder = new AlertDialog.Builder(bg4_3question.this);
+               builder.setTitle("請輸入問題與答案:")
+                       .setIcon(R.mipmap.ic_launcher)
+                       .setMessage("範例:\nQ:結婚紀念日是什麼時候?\nA:8/31")
+                       .setNegativeButton("關閉", null)
+                       .show();
+           }
+       });
     }
-    /*private Button.OnClickListener listener= new Button.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(view.getId()==R.id.bt6){
 
-            }
-        }
-    };*/
 }
